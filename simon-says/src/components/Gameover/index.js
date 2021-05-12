661392ff076b91d40de4f59db60efabec33bf94d
Maxx Sanner <maxxsanner105@gmail.com>
2021-05-12 13:42:10 -0700

    added high scores

--- simon-says/src/components/Gameover/index.js
+++ simon-says/src/components/Gameover/index.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import HighscoreModal from "../HighscoreModal";
+import Highscores from "../Highscores";
 import "./style.css";
 
 
 function Gameover(props) {
+    const [viewScores, setViewScores] = useState(false);
+
+    function handleViewScores() {
+        setViewScores(true);
+    }
+
+    function handleExitViewScores() {
+        setViewScores(false);
+    }
     return (
         <div>
             <h3>{props.gameoverMsg}</h3>
+            <h3> You Scored <strong>{props.score}</strong>!</h3>
             <form>
-                <button className="btn btn-secondary">Start Over</button> <br/>
+                <button className="btn btn-secondary">Start Over</button> 
+                <br/>
             </form>
             <button type="button" className="btn btn-secondary" data-toggle="modal" data-target="#bookSelect" id="submit-score">
                 Submit Score
             </button>
+            <br/>
+            <button className="btn btn-secondary" onClick = {handleViewScores}>View Scores</button>
+            {viewScores ? (<Highscores handleExitViewScores = {handleExitViewScores}></Highscores>) : null}
+            
            <HighscoreModal score={props.score}></HighscoreModal>
         </div>
 
--- simon-says/src/components/Highscores/index.js
+++ simon-says/src/components/Highscores/index.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./style.css";
 
 
 function Highscores(props) {
+    const [scores, setScores] = useState([]);
+
+    useEffect(() => {
+        setScores(JSON.parse(localStorage.getItem('score')))
+    }, []);
 
     return (
-        <div>
-            
+        <div style={{margin: "20px"}}>
+            <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Initials</th>
+                    <th scope="col">Score</th>
+                </tr>
+            </thead>
+            <tbody>
+                {scores.map((score) => (
+                    <tr>
+                        <th scope="row">{scores.indexOf(score) + 1}</th>
+                        <td>{score.initials}</td>
+                        <td>{score.score}</td>
+                    </tr>
+                                
+                ))} 
+            </tbody>
+            </table>
+            <button onClick = {props.handleExitViewScores} className="btn btn-danger">X</button>
         </div>
-
     )
 }
 
